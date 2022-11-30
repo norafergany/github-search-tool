04e4a87dfab804396a60c91d589c7fbbef9f6c88
Nora Fergany <nora.fergany@gmail.com>
2022-11-30 18:24:12 -0500

    Update SearchResults.test.js to reflect new implementation and test more cases
    * No user found
    * Non-default user search

--- src/components/SearchResults.test.js
+++ src/components/SearchResults.test.js
@@ -4,10 +4,10 @@ import {
 
 import * as api from "../api/API.js";
 import SearchResults from "./SearchResults";
+import {act} from "react-dom/test-utils";
 
 
 jest.mock("../api/API.js")
-afterEach(cleanup);
 
 it("Display nora's profile when component mounts", async () => {
 
@@ -15,7 +15,6 @@ it("Display nora's profile when component mounts", async () => {
     const nullText = "Not Available"
 
     const defaultUsername = "norafergany";
-
     const defaultProfile = {
         avatar_url: "https://avatars.githubusercontent.com/u/25270812?v=4",
         html_url: "https://github.com/norafergany",
@@ -35,13 +34,17 @@ it("Display nora's profile when component mounts", async () => {
         stringDate: "21 Jan 2017",
     };
 
-    api.getProfile.mockResolvedValueOnce(defaultProfile);
-    render(<SearchResults/>);
-    const matched = await screen.findByText(/Nora Fergany/);
-    expect(matched).toBeInTheDocument();
+    await act(() => {
+        render(<SearchResults profile={defaultProfile} username={defaultUsername}
+                              error={false}
+        />)
+    })
+
+    const githubName = await screen.findByText(/Nora Fergany/);
+    expect(githubName).toBeInTheDocument();
 
     const image = await screen.getByAltText('avatar');
-    expect(image.src).toContain('https://avatars.githubusercontent.com/u/25270812?v=4');
+    expect(image.src).toContain(`${defaultProfile.avatar_url}`);
 
     expect(screen.getByText(noBioText).toBeInTheDocument);
     expect(document.querySelector("a").getAttribute("href")).toBe(defaultProfile.html_url);
@@ -64,4 +67,76 @@ it("Display nora's profile when component mounts", async () => {
     const following = await screen.getByTestId("following");
     expect(following).toHaveTextContent(defaultProfile.following);
 
+})
+
+it("Displays the error message when the username doesn't exist", async() => {
+
+    const notFoundUser = "abdhgdsgfhds";
+
+    await act(() => {
+        render(<SearchResults profile={undefined} username={notFoundUser}
+                              error={true}
+        />)
+    })
+    const errorMessage = await screen.findByText(/No results/i);
+    expect(errorMessage).toBeInTheDocument();
+})
+
+it("Display the username's Github profile when someone makes a search", async () => {
+
+    const noBioText = "This profile has no bio";
+    const nullText = "Not Available"
+
+    const octocatUsername = "octocat";
+    const octocatProfile = {
+        avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
+        html_url: "https://github.com/octocat",
+        name: "The Octocat",
+        company: "@github",
+        blog: "https://github.blog",
+        location: "San Francisco",
+        bio: null,
+        twitter_username: null,
+        public_repos: 8,
+        followers: 7670,
+        following: 9,
+        created_at: "2011-01-25T18:44:36Z",
+        githubURL:`https://github.com/${octocatUsername}`,
+        companyURL:"https://github.com/github",
+        twitterURL: null,
+        stringDate: "25 Jan 2011",
+    };
+
+    await act(() => {
+        render(<SearchResults profile={octocatProfile} username={octocatUsername}
+                              error={false}
+        />)
+    })
+
+    const githubName = await screen.findByText(`${octocatProfile.name}`);
+    expect(githubName).toBeInTheDocument();
+
+    const image = await screen.getByAltText('avatar');
+    expect(image.src).toContain(`${octocatProfile.avatar_url}`);
+
+    expect(screen.getByText(noBioText).toBeInTheDocument);
+    expect(document.querySelector("a").getAttribute("href")).toBe(octocatProfile.html_url);
+
+    const nullValues = screen.getAllByText(nullText);
+    expect(nullValues).toHaveLength(1);
+
+    const blogDiv = screen.getByTestId("blog");
+    expect(blogDiv).toHaveTextContent(`${octocatProfile.blog}`);
+
+    const location = await screen.getByTestId("location");
+    expect(location).toHaveTextContent(octocatProfile.location);
+
+    const repos = await screen.getByTestId("repos");
+    expect(repos).toHaveTextContent(octocatProfile.public_repos);
+
+    const followers = await screen.getByTestId("followers");
+    expect(followers).toHaveTextContent(octocatProfile.followers);
+
+    const following = await screen.getByTestId("following");
+    expect(following).toHaveTextContent(octocatProfile.following);
 })
